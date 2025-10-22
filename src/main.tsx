adeb162cdebcd0c92d5e8360c6d489c715f32957
Usman <161955641+usmank06@users.noreply.github.com>
2025-10-22 15:17:31 -0500

    Remove all source files and project configuration

--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import GraphCanvas from './GraphCanvas'
-
-const el = document.getElementById('root')!
-createRoot(el).render(<GraphCanvas />)
-
